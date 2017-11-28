3031e6cdda9757386589b9d42edcc8de49129b23
Simen Fjellstad <zaimennor@gmail.com>
2017-11-28 16:25:21 +0100

    Adds alpha to colorFromHexString function

--- lib/ios/AirGoogleMaps/AIRGoogleMapHeatmap.m
+++ lib/ios/AirGoogleMaps/AIRGoogleMapHeatmap.m
@@ -60,7 +60,11 @@
   NSScanner *scanner = [NSScanner scannerWithString:hexString];
   [scanner setScanLocation:1]; // bypass '#' character
   [scanner scanHexInt:&rgbValue];
-  return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+  int alpha = ((rgbValue & 0xFF000000) >> 24) / 255.0;
+  int red = ((rgbValue & 0xFF0000) >> 16) / 255.0;
+  int green = ((rgbValue & 0xFF00) >> 8) / 255.0;
+  int blue = (rgbValue & 0xFF)/255.0;
+  return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
 }
 
 - (void)setGradient:(NSDictionary *)gradient {
