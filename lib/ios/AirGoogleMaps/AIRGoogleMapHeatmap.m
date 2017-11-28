1e3cbc2d19e44dece358a042030531fa9ba47f13
Simen Fjellstad <zaimennor@gmail.com>
2017-11-28 16:52:10 +0100

    Fixes alpha function

--- lib/ios/AirGoogleMaps/AIRGoogleMapHeatmap.m
+++ lib/ios/AirGoogleMaps/AIRGoogleMapHeatmap.m
@@ -54,17 +54,46 @@
 
   [_heatmap clearTileCache];
 }
-
-- (UIColor *)colorFromHexString:(NSString *)hexString {
-  unsigned rgbValue = 0;
-  NSScanner *scanner = [NSScanner scannerWithString:hexString];
-  [scanner setScanLocation:1]; // bypass '#' character
-  [scanner scanHexInt:&rgbValue];
-  int alpha = ((rgbValue & 0xFF000000) >> 24) / 255.0;
-  int red = ((rgbValue & 0xFF0000) >> 16) / 255.0;
-  int green = ((rgbValue & 0xFF00) >> 8) / 255.0;
-  int blue = (rgbValue & 0xFF)/255.0;
-  return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+- (CGFloat) colorComponentFrom: (NSString *) string start: (NSUInteger) start length: (NSUInteger) length {
+  NSString *substring = [string substringWithRange: NSMakeRange(start, length)];
+  NSString *fullHex = length == 2 ? substring : [NSString stringWithFormat: @"%@%@", substring, substring];
+  unsigned hexComponent;
+  [[NSScanner scannerWithString: fullHex] scanHexInt: &hexComponent];
+  return hexComponent / 255.0;
+}
+- (UIColor *) colorWithHexString: (NSString *) hexString {
+  NSString *colorString = [[hexString stringByReplacingOccurrencesOfString: @"#" withString: @""] uppercaseString];
+  CGFloat alpha, red, blue, green;
+  switch ([colorString length]) {
+    case 3: // #RGB
+      alpha = 1.0f;
+      red   = [self colorComponentFrom: colorString start: 0 length: 1];
+      green = [self colorComponentFrom: colorString start: 1 length: 1];
+      blue  = [self colorComponentFrom: colorString start: 2 length: 1];
+      break;
+    case 4: // #ARGB
+      alpha = [self colorComponentFrom: colorString start: 0 length: 1];
+      red   = [self colorComponentFrom: colorString start: 1 length: 1];
+      green = [self colorComponentFrom: colorString start: 2 length: 1];
+      blue  = [self colorComponentFrom: colorString start: 3 length: 1];
+      break;
+    case 6: // #RRGGBB
+      alpha = 1.0f;
+      red   = [self colorComponentFrom: colorString start: 0 length: 2];
+      green = [self colorComponentFrom: colorString start: 2 length: 2];
+      blue  = [self colorComponentFrom: colorString start: 4 length: 2];
+      break;
+    case 8: // #AARRGGBB
+      alpha = [self colorComponentFrom: colorString start: 0 length: 2];
+      red   = [self colorComponentFrom: colorString start: 2 length: 2];
+      green = [self colorComponentFrom: colorString start: 4 length: 2];
+      blue  = [self colorComponentFrom: colorString start: 6 length: 2];
+      break;
+    default:
+      [NSException raise:@"Invalid color value" format: @"Color value %@ is invalid.  It should be a hex value of the form #RBG, #ARGB, #RRGGBB, or #AARRGGBB", hexString];
+      break;
+  }
+  return [UIColor colorWithRed: red green: green blue: blue alpha: alpha];
 }
 
 - (void)setGradient:(NSDictionary *)gradient {
@@ -73,8 +102,13 @@
   NSMutableArray<UIColor *> *colors = [NSMutableArray arrayWithCapacity:[rawColors count]];
   NSMutableArray<NSNumber *> *values = [NSMutableArray arrayWithCapacity:[rawValues count]];
   for(int i = 0; i < [rawColors count]; i++){
-    [colors addObject:[self colorFromHexString:rawColors[i]]];
+    [colors addObject:[self colorWithHexString:rawColors[i]]];
     [values addObject:rawValues[i]];
+    NSLog(@"COLOR ENTRY:");
+    NSLog(@"%@", rawColors[i]);
+    NSLog(@"%@", rawValues[i]);
+    NSLog(@"%@", colors[i]);
+    NSLog(@"%@", values[i]);
   }
   GMUGradient *gmuGradient =[[GMUGradient alloc] initWithColors:colors
                                                    startPoints:values
